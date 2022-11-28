40e61d5ab687d046a5d096d1066f0d0ea6b56ac0
Nik <nik@niktek.io>
2022-11-28 13:10:33 +1100

    Removed test

--- atest/tailwind.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', require('path').join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
-	theme: {
-		extend: {},
-	},
-	plugins: [require('@skeletonlabs/skeleton/tailwind/theme.cjs')],
-}
