1f00aac9585de7f626053df865abe58efcc67c55
Konstantinos Kostarellis <delight@users.noreply.github.com>
2023-01-10 14:27:20 +0100

    container div should not span around style

--- templates/welcome/src/routes/+page.svelte
+++ templates/welcome/src/routes/+page.svelte
@@ -23,24 +23,24 @@
 			<p><code>/src/routes/+page.svelte</code></p>
 		</div>
 	</div>
-
-	<style lang="postcss">
-		figure {
-			@apply flex relative flex-col;
-		}
-		figure svg,
-		.img-bg {
-			@apply w-64 h-64 md:w-80 md:h-80;
-		}
-		.img-bg {
-			@apply bg-gradient-to-r from-primary-300 to-warning-300;
-			@apply absolute z-[-1] rounded-full blur-[64px];
-			animation: pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite;
-		}
-		@keyframes pulse {
-			50% {
-				transform: scale(1.5);
-			}
-		}
-	</style>
 </div>
+
+<style lang="postcss">
+	figure {
+		@apply flex relative flex-col;
+	}
+	figure svg,
+	.img-bg {
+		@apply w-64 h-64 md:w-80 md:h-80;
+	}
+	.img-bg {
+		@apply bg-gradient-to-r from-primary-300 to-warning-300;
+		@apply absolute z-[-1] rounded-full blur-[64px];
+		animation: pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite;
+	}
+	@keyframes pulse {
+		50% {
+			transform: scale(1.5);
+		}
+	}
+</style>
