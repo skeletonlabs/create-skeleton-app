bbe7728453f4c241f2d6461e765cbdaa61065ab6
Konstantinos Kostarellis <delight@users.noreply.github.com>
2023-01-24 23:37:31 +0100

    update for skeleton v0.99.5
    need to use variant since v0.99.5

--- templates/welcome/src/routes/+layout.svelte
+++ templates/welcome/src/routes/+layout.svelte
@@ -14,9 +14,9 @@
 				<strong class="text-xl uppercase">Skeleton</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a class="btn btn-sm btn-ghost-surface" href="https://discord.gg/EXqV7W8MtY" target="_blank" rel="noreferrer">Discord</a>
-				<a class="btn btn-sm btn-ghost-surface" href="https://twitter.com/SkeletonUI" target="_blank" rel="noreferrer">Twitter</a>
-				<a class="btn btn-sm btn-ghost-surface" href="https://github.com/skeletonlabs/skeleton" target="_blank" rel="noreferrer">GitHub</a>
+				<a class="btn btn-sm variant-ghost-surface" href="https://discord.gg/EXqV7W8MtY" target="_blank" rel="noreferrer">Discord</a>
+				<a class="btn btn-sm variant-ghost-surface" href="https://twitter.com/SkeletonUI" target="_blank" rel="noreferrer">Twitter</a>
+				<a class="btn btn-sm variant-ghost-surface" href="https://github.com/skeletonlabs/skeleton" target="_blank" rel="noreferrer">GitHub</a>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
