9e9b7279852c3adf938e869d840e5e105fea5690
Nik <nik@niktek.io>
2023-05-10 10:39:28 +1000

    Do the actual changes required....

--- templates/welcome/src/routes/+page.svelte
+++ templates/welcome/src/routes/+page.svelte
@@ -2,7 +2,7 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
-		<h2 class="font-bold">Welcome to Skeleton.</h2>
+		<h2 class="h2 font-bold">Welcome to Skeleton.</h2>
 		<!-- Animated Logo -->
 		<figure>
 			<section class="img-bg" />
@@ -30,8 +30,8 @@
 		</div>
 		<div class="space-y-2">
 			<p>Try editing the following:</p>
-			<p><code>/src/routes/+layout.svelte</code></p>
-			<p><code>/src/routes/+page.svelte</code></p>
+			<p><code class="code">/src/routes/+layout.svelte</code></p>
+			<p><code class="code">/src/routes/+page.svelte</code></p>
 		</div>
 	</div>
 </div>
